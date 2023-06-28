93091261d328d5fb18dab1e0b5b73bb9a9440568
Mehmet Ali KOÇAL <root.gwyn@hotmail.com>
2023-06-28 07:43:23 +0300

    toast message component added to the root

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,8 +1,26 @@
 import '@/styles/globals.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function App({
   Component,
   pageProps,
 }) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Component {...pageProps} />{' '}
+      <ToastContainer
+        position='bottom-right'
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={true}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme='light'
+      />
+    </div>
+  );
 }
