61ef6b53179f27b72b1988f7ce1a1bb1bab69081
Mehmet Ali KOÇAL <root.gwyn@hotmail.com>
2023-06-28 07:41:32 +0300

    email validation, responsive design and toast message feedback added

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
+import { useRef } from 'react';
 import { useState } from 'react';
-import { Montserrat } from 'next/font/google';
 import { useRouter } from 'next/router';
-import Visibility from '../components/icons/visibility';
+import { Montserrat } from 'next/font/google';
+import { toast } from 'react-toastify';
+import PasswordVisibility from '../components/icons/passwordVisibility';
 
 const montserrat = Montserrat({
   subsets: ['latin'],
@@ -13,17 +15,55 @@ const inputs = [
     type: 'email',
     label: 'Email',
     placeholder: 'Enter your email',
+    regexp: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/,
   },
   {
     id: 'password',
     type: 'password',
     label: 'Password',
     placeholder: 'Enter your password',
+    regexp: /^[\w\W]+$/,
   },
 ];
 
 export default function Home() {
-const router = useRouter();
+  const router = useRouter();
+  const email = useRef(null);
+  const password = useRef(null);
+
+  function isEmailValid() {
+    const isValid =
+      /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(
+        email.current.value
+      );
+
+    if (!isValid) {
+      toast.error(
+        'Please enter a valid email adress'
+      );
+    }
+    return isValid;
+  }
+
+  function isPasswordValid() {
+    const isValid = /^[\w\W]+$/.test(
+      password.current.value
+    );
+
+    if (!isValid) {
+      toast.error('Your password is incorrect');
+    }
+    return isValid;
+  }
+
+  function login() {
+    if (isEmailValid() && isPasswordValid()) {
+      toast.success(
+        "You've successfully logged in"
+      );
+      router.push('/dashboard');
+    }
+  }
 
   const [
     isPasswordVisible,
@@ -32,36 +72,41 @@ const router = useRouter();
 
   return (
     <main
-      className={`w-screen h-screen flex justify-center items-center bg-gradient-49deg from-themeColor to-themeColor-lighter ${montserrat.className}`}
+      className={`w-screen h-screen flex py-20 justify-center items-center overflow-x-hidden bg-gradient-49deg from-themeColor to-themeColor-lighter ${montserrat.className}`}
     >
       <form
         onSubmit={() => {
           event.preventDefault();
         }}
-        className='w-[475px] h-[550px] bg-white text-black rounded-[20px] flex flex-col justify-center items-center shadow-loginForm flex-shrink-0'
+        className='w-[475px] h-[550px] max-xs:px-4 px-8 bg-white text-black xs:rounded-[20px] flex flex-col justify-center items-center shadow-loginForm'
       >
-        <h1 className='pl-2 text-3.5xl h-10 flex justify-center items-center font-bold uppercase border-l-6 border-themeColor-lighter'>
+        <h1 className='pl-3 text-3.5xl max-xs:text-2xl h-10 flex justify-center items-center font-bold uppercase border-l-6 border-themeColor-lighter text-center'>
           Manage Courses
         </h1>
-        <h2 className='mt-12 text-2xl font-semibold uppercase'>
+        <h2 className='mt-12 max-xs:mt-8 text-2xl max-xs:text-lg font-semibold uppercase text-center'>
           Sign In
         </h2>
-        <p className='mt-1 text-sm text-fadedTextColor font-medium'>
+        <p className='mt-1 text-sm max-xs:text-xs text-fadedTextColor font-medium text-center'>
           Enter your credentials to access your
           account
         </p>
-        <div className='px-8 mt-10 w-full flex flex-col gap-y-4'>
+        <div className='mt-10 max-xs:mt-6 w-full flex flex-col gap-y-4'>
           {inputs.map((input) => (
             <div key={input.id}>
               <label
                 htmlFor={input.id}
-                className='text-sm font-medium text-fadedTextColor'
+                className='text-sm max-xs:text-xs font-medium text-fadedTextColor'
               >
                 {input.label}
               </label>
               <div className='flex w-full mt-1.5 relative items-center border-1 rounded-md hover:border-themeColor-lighter/60 border-loginFormBorderColor'>
                 <input
                   id={input.id}
+                  ref={
+                    input.id === 'password'
+                      ? password
+                      : email
+                  }
                   type={
                     input.id === 'password' &&
                     !isPasswordVisible
@@ -69,6 +114,7 @@ const router = useRouter();
                       : 'text'
                   }
                   placeholder={input.placeholder}
+                  autoComplete='on'
                   className={`p-4 h-11 flex-shrink-0 rounded-md outline-none text-sm transition-all duration-200 placeholder:text-xs placeholder:text-inputPlaceholderColor ${
                     input.id === 'password'
                       ? 'w-5/6'
@@ -84,16 +130,26 @@ const router = useRouter();
                       );
                     }}
                   >
-                    <Visibility className='z-10 h-11 right-0 w-full flex justify-center items-center cursor-pointer border-l-1 border-loginFormBorderColor transition-all duration-200 hover:bg-themeColor rounded-r-md' />
+                    <PasswordVisibility className='z-10 h-11 right-0 w-full flex justify-center items-center cursor-pointer border-l-1 border-loginFormBorderColor transition-all duration-200 hover:bg-themeColor rounded-r-md' />
                   </div>
                 )}
               </div>
             </div>
           ))}
-          <button onClick={() => { router.push('/dashboard') }} className='p-3.5 w-full mt-2 text-sm uppercase font-medium text-white rounded-[4px] bg-themeColor flex-shrink-0 transition-all duration-200 active:scale-95 hover:bg-themeColor/80'>
+          <button
+            onClick={() => {
+              login();
+            }}
+            className='p-3.5 w-full mt-2 text-sm max-xs:text-xs uppercase font-medium text-white rounded-[4px] bg-themeColor flex-shrink-0 transition-all duration-200 active:scale-95 hover:bg-themeColor/80'
+          >
             Sign In
           </button>
-          <p className='mt-2 text-sm text-center text-fadedTextColor'>Forgot your password? <button className='text-themeColor underline hover:text-themeColor/80 transition-all duration-200 active:scale-95'>Reset Password</button></p>
+          <p className='mt-2 text-sm max-xs:text-xs text-center text-fadedTextColor'>
+            Forgot your password?{' '}
+            <button className='text-themeColor underline hover:text-themeColor/80 transition-all duration-200 active:scale-95'>
+              Reset Password
+            </button>
+          </p>
         </div>
       </form>
     </main>
