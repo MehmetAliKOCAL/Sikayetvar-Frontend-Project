ec578977733433cbf81d081f2c02d85b3b500a0c
Mehmet Ali KOÇAL <root.gwyn@hotmail.com>
2023-07-01 22:47:08 +0300

    initial commit

---
+++ src/components/icons/additionIcon.js
@@ -0,0 +1,17 @@
+export default function AdditionIcon({ className }) {
+  return (
+    <svg
+      fill='none'
+      viewBox='0 0 24 24'
+      strokeWidth='1.5'
+      stroke='currentColor'
+      className={className}
+    >
+      <path
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        d='M12 6v12m6-6H6'
+      />
+    </svg>
+  );
+}
